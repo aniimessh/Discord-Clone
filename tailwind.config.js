638de4d752838cfcdc293da05cf800d9bc3c7702
Animesh Gupta <97246133+aniimessh@users.noreply.github.com>
2023-02-02 14:08:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        fontFamily: {
+          ggsansBold : ["ggsansBold"],
+          ggsansMedium: ["ggsansMedium"],
+          ggsansNormal : ["ggsansNormal"] ,         
+          ggsansSemiBold : ["ggsansSemiBold"],
+          GintoNordNormal : ["GintoNordNormal"], 
+          GintoNordNormal2: ["GintoNordNormal2"],
+          GintoNordSemiBold :["GintoNordSemiBold"],
+          GintoNordSemiBold2  : ["intoNordSemiBold2"],
+          GintoNordBold  : [" GintoNordBold"],
+          GintoNordBold2  : ["GintoNordBold2"],
+          GintoItalicNormal  : ["GintoItalicNormal"],
+          GintoNormal  : ["GintoNormal"],
+          GintoItalicMedium  : ["GintoItalicMedium"],
+          GintoMedium : ["GintoMedium"],
+          GintoItalicSemiBold  : ["GinntoItalicSemiBold"],
+          GintoSemiBold :["GintoSemiBold"],
+          GintoBold  : ["GintoBold"],
+          GintoItalicBold  : ["GintoItalicBold"],
+          GintoExBold  : ["GintoExBold"],
+          GintoItalicExBold  : ["GintoItalicExBold"],
+          WhitneyThin : ["WhitneyThin"],
+          WhitneyNormal  : ["WhitneyNormal"],
+          WhitneyMedium  : ["WhitneyMedium"],
+          WhitneySemiBold  : ["WhitneySemiBold"],
+          WhitneyBold  : ["WhitneyBold"],
+      }
+    },
+  },
+  plugins: [],
+}
+}
